342a6205ae5cb87b96118039a945e1dc2ee99428
Arturs Aboltins <arthuraboltins@gmail.com>
2023-03-09 20:22:40 +0000

    add engineer/intern inquirer questions

--- index.js
+++ index.js
@@ -58,18 +58,59 @@ const promptForNextEmployee = () => {
 }
 
 const promptForEngineer = () => {
-    inquirer.prompt([{
+    inquirer.prompt([
         //engineer questions
-    }]).then(response => {
-        // add new engineer to employees array
-        // promptForNextEmployee
-    })
+        {
+            type: 'input',
+            message: "What is your engineer's name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's id?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's email?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's Github username?",
+            name: 'github',
+        },
+        {
+        }]).then(response => {
+            // add new engineer to employees array
+            promptForNextEmployee();
+        })
 }
 
 const promptForIntern = () => {
-    inquirer.prompt([{
+    inquirer.prompt([
         //intern questions
-    }]).then(response => {
+        {
+            type: 'input',
+            message: "What is your intern's name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is your intern's id?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is your intern's email?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is your intern's school?",
+            name: 'school',
+        },
+    ]).then(response => {
         // add new intern to employees array
         // promptForNextEmployee
     })
